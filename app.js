a9bdf8a73b50bfd0a1de5fb1b0df7f8604710222
alexxxua <aluaex@gmail.com>
2014-09-28 14:34:35 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,345 @@
+﻿var querystring	= require('querystring'),
+	express		= require('express'),
+	app			= express(),
+	fs 			= require("fs"),
+	mime 		= require('mime');
+
+var mongo;
+app.configure('development', function(){
+    mongo = {
+        "hostname":"localhost",
+        "port":27017,
+        "username":"",
+        "password":"",
+        "name":"",
+        "db":"db"
+    }
+});
+app.configure('production', function(){
+    var env = JSON.parse(process.env.VCAP_SERVICES);
+    mongo = env['mongodb-1.8'][0]['credentials'];
+});
+var generate_mongo_url = function(obj){
+    obj.hostname = (obj.hostname || 'localhost');
+    obj.port = (obj.port || 27017);
+    obj.db = (obj.db || 'test');
+    if(obj.username && obj.password){
+        return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
+    }else{
+        return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
+    }
+}
+var mongourl = generate_mongo_url(mongo);
+
+var ObjectID = require('mongodb').ObjectID;
+
+//set path to the views (template) directory
+app.set('views', __dirname + '/views');
+
+//set path to static files
+app.use(express.static(__dirname+ '/static',{maxAge : new Date(Date.now() + 900000000)}));
+
+//POST
+app.use(express.bodyParser());
+
+//Cookie
+app.use(express.cookieParser());
+
+//Remove "Rooms" collection
+
+// require('mongodb').connect(mongourl, function(err, db){
+// 		db.collection('rooms').drop();
+// });
+
+function calcTime(offset) {
+	d = new Date();
+	utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+	nd = new Date(utc + (3600000*offset));
+
+	return nd;
+}
+
+
+//Handle GET requests on '/'
+app.get('/', function(req, res){
+	require('mongodb').connect(mongourl, function(err, db){
+		db.collection('rooms', function(err, collection){
+			collection.find({}).toArray(function(err, results){
+				res.render('index.jade', {'addAttrs' : addAttrs, 'rooms' : results});
+			})
+		})
+	})
+
+});
+app.get('/addRoom', function(req, res){
+	var d = calcTime(req.cookies.timeZone),
+		curDate = d.getDate(),
+		curMonth = d.getMonth()+1,
+		curYear = d.getFullYear(),
+		curDate = (curMonth<=9? '0'+curMonth : curMonth)+'/'+(curDate<=9? '0'+curDate : curDate)+'/'+curYear;
+
+	require('mongodb').connect(mongourl, function(err, db){
+		db.collection('rooms', function(err, collection){
+			collection.insert({
+				town: req.query.town.val,
+				region: req.query.region.val,
+				street: req.query.street.val,
+				numbRoom: +req.query.numbRoom.val,
+				floor: +req.query.floor.val,
+				buildFloors: +req.query.buildFloors.val,
+				space: +req.query.space.val,
+				livingSpace: +req.query.livingSpace.val,
+				kitchenSpace: +req.query.kitchenSpace.val,
+				balcony: +req.query.balcony.val,
+				bathroom: req.query.bathroom.val,
+				condition: req.query.condition.val,
+				price: +req.query.price.val,
+				auction: req.query.auction.val,
+				saleStage : req.query.saleStage.val,
+				notes: req.query.notes.val,
+				publishDate : {mSec : d, faoormatedDate : curDate}
+			}, function(err, insertedData){
+				res.send(insertedData[0]);
+			})
+		})
+	})
+});
+
+//Delete room
+app.get('/delRoom', function(req, res){
+	require('mongodb').connect(mongourl, function(err, db){
+		db.collection('rooms', function(err, collection){
+			collection.remove({_id : ObjectID(req.query.delID)});
+			res.send('Deleted');
+		})
+	})
+})
+
+//Edit room
+app.get('/editRoom', function(req, res){
+	require('mongodb').connect(mongourl, function(err, db){
+		db.collection('rooms', function(err, collection){
+			collection.update({_id : ObjectID(req.query.id.val)},{ $set: {
+				town: req.query.town.val,
+				region: req.query.region.val,
+				street: req.query.street.val,
+				numbRoom: +req.query.numbRoom.val,
+				floor: +req.query.floor.val,
+				buildFloors: +req.query.buildFloors.val,
+				space: +req.query.space.val,
+				livingSpace: +req.query.livingSpace.val,
+				kitchenSpace: +req.query.kitchenSpace.val,
+				balcony: +req.query.balcony.val,
+				bathroom: req.query.bathroom.val,
+				condition: req.query.condition.val,
+				price: +req.query.price.val,
+				auction: req.query.auction.val,
+				saleStage: req.query.saleStage.val,
+				notes: req.query.notes.val
+			}});
+			res.send(req.query);
+		})
+	})
+})
+
+//Search room
+app.get('/searchRoom', function(req, res){
+	var searchObject = {};
+
+	//Build search object
+	for(var item in addAttrs){
+		if(addAttrs[item].searchable){
+			if(addAttrs[item].searchableType == 'one' && req.query[item].val){
+				searchObject[item] = {$regex: req.query[item].val, $options:'i'};
+			}
+			else if(addAttrs[item].searchableType == 'range'){
+				if(req.query[item+'From'].val && req.query[item+'To'].val)
+					searchObject[item] = {$gte: +req.query[item+'From'].val, $lte: +req.query[item+'To'].val};
+				else if(req.query[item+'From'].val)
+					searchObject[item] = +req.query[item+'From'].val;
+				else if(req.query[item+'To'].val)
+					searchObject[item] = {$gte: 0, $lte: +req.query[item+'To'].val};
+			}
+		}
+	}
+
+	require('mongodb').connect(mongourl, function(err, db){
+		db.collection('rooms', function(err, collection){
+			collection.find(searchObject).toArray(function(err, searchResult){
+				res.cookie('searchObj', searchResult, { path: '/', expires: new Date(Date.now() + 900000000), httpOnly: true });
+				res.send(searchResult);
+			});
+		});
+	});
+});
+
+//Page 404
+app.get('/error404', function(req, res){
+	res.render('error404.jade');
+});
+
+//Get file
+app.get('*', function(req, res){
+	fs.readFile(__dirname + req.originalUrl, 'utf8', function(err, text){
+		var type = mime.lookup(__dirname + req.originalUrl);
+		if(!err){
+			if (!res.getHeader('Content-Type')) {
+			  	var charset = mime.charsets.lookup(type);
+				res.setHeader('Content-Type', type + (charset ? '; charset=' + charset : ''));
+        		res.send(text);
+			}
+		}
+		else{
+			res.redirect('/error404');
+		}
+    });
+});
+var addAttrs = {
+	town: {
+		id:'town',
+		label: 'Город',
+		type: 'text',
+		dataValidate: 'notEmpty',
+		dataValidateMessage: 'Заполните поле',
+		searchable: true,
+		searchableType: 'one'
+	},
+	region: {
+		id:'region',
+		label: 'Район',
+		type: 'text',
+		dataValidate: 'none',
+		searchable: true,
+		searchableType: 'one'
+	},
+	street: {
+		id: 'street',
+		label: 'Улица',
+		type: 'text',
+		dataValidate: 'notEmpty',
+		dataValidateMessage: 'Заполните пожалуйста улицу',
+		searchable: true,
+		searchableType: 'one'
+	}, 
+	numbRoom: {
+		id:'numbRoom' ,
+		label: 'Кол. комнат',
+		type: 'text',
+		dataValidate: 'numb',
+		dataValidateMessage: 'Заполните количество комнат',
+		searchable: true,
+		searchableType: 'range'
+	},
+	floor: {
+		id:'floor' ,
+		label: 'Этаж',
+		type: 'text',
+		dataValidate: 'numb',
+		dataValidateMessage: 'Заполните этаэ',
+		searchable: true,
+		searchableType: 'range'
+	},
+	buildFloors: {
+		id:'buildFloors' ,
+		label: 'Кол. этажей в доме',
+		type: 'text',
+		dataValidate: 'numb',
+		dataValidateMessage: 'Заполните количество этажей в доме',
+		searchable: true,
+		searchableType: 'range'
+	},
+	space: {
+		id:'space' ,
+		label: 'Площадь',
+		type: 'text',
+		dataValidate: 'floatNumb',
+		dataValidateMessage: 'Заполните общую площадь квартиры',
+		searchable: true,
+		searchableType: 'range'
+	},
+	livingSpace: {
+		id:'livingSpace' ,
+		label: 'Жилая площадь',
+		type: 'text',
+		dataValidate: 'floatNumb',
+		dataValidateMessage: 'Заполните жилую площадь квартиры',
+		searchable: true,
+		searchableType: 'range'
+	},
+	kitchenSpace: {
+		id:'kitchenSpace' ,
+		label: 'Кухонная площадь',
+		type: 'text',
+		dataValidate: 'floatNumb',
+		dataValidateMessage: 'Заполните кухонную площадь',
+		searchable: true,
+		searchableType: 'range'
+	},
+	balcony: {
+		id:'balcony' ,
+		label: 'Кол. балконов',
+		type: 'text',
+		dataValidate: 'numb',
+		dataValidateMessage: 'Заполните количесвто балконов',
+		searchable: true,
+		searchableType: 'range'
+	},
+	bathroom: {
+		id:'bathroom' ,
+		label: 'Санузел',
+		type: 'select',
+		options: ['Выбирите тип', 'совмещенный', 'разделенный'],
+		dataValidate: 'notEmpty',
+		dataValidateMessage: 'Выбирите тип санузла',
+		searchable: true,
+		searchableType: 'one'
+	},
+	condition: {
+		id:'condition' ,
+		label: 'Состояние квартиры',
+		type: 'select',
+		options: ['Выбирите состояние', 'отличное', 'хорошое', 'среднее', 'плохое'],
+		dataValidate: 'notEmpty',
+		dataValidateMessage: 'Выбирете состояние квартиры',
+		searchable: true,
+		searchableType: 'one'
+	},
+	price: {
+		id:'price' ,
+		label: 'Цена в $',
+		type: 'text',
+		dataValidate: 'floatNumb',
+		dataValidateMessage: 'Заполните цену в числовом формате',
+		searchable: true,
+		searchableType: 'range'
+	},
+	auction: {
+		id:'auction' ,
+		label: 'Торг',
+		type: 'select',
+		options: ['Выбирите возможность','возможен', 'невозможен'],
+		dataValidate: 'notEmpty',
+		dataValidateMessage: 'Выбирете возможность торга',
+		searchable: true,
+		searchableType: 'one'
+	},
+	saleStage: {
+		id : 'saleStage',
+		label: 'Етап продажи',
+		type: 'select',
+		options: ['Выбирете етап', 'Продается', 'Ожидание взноса', 'Продана'],
+		dataValidate: 'notEmpty',
+		dataValidateMessage: 'Выбирете етап продажи',
+		searchable: true,
+		searchableType: 'one'
+	},
+	notes: {
+		id:'notes' ,
+		label: 'Заметки',
+		type: 'textarea',
+		dataValidate: 'none',
+		searchable: false
+	}
+};
+//listen on localhost 8888
+app.listen(8000);
---
+++ static/js/js.js
@@ -0,0 +1,336 @@
+﻿(function( $, undefined ) {
+	"use strict"
+	$(document).ready(function(){
+		var $loadingItem = $('#loading');
+
+		// Datepicker
+		// $("#datepicker").datepicker({
+		// 	onSelect : setFocusInp
+		// });
+		// $("#datepicker2").datepicker({
+		// 	onSelect : setFocusInp
+		// });
+
+		//Table sorter
+		//.tablesorter();
+
+		//Placeholders
+		//$('input[placeholder], textarea[placeholder]').placeholder();
+
+		//Get and set time in cookie
+		function setTimeCookie(){
+			var	d = new Date();
+			//Time zone
+			var	timeZone = -d.getTimezoneOffset()/60;
+			//Record timezone in cookies
+			$.cookie('timeZone', timeZone);
+		}
+		setTimeCookie();
+
+		//Flour number
+		function floorN(x, n){
+			var numb = Number(x);
+			return numb.toFixed(n);
+		}
+
+		//Regexp
+		var regexp = {
+			numb : /^\b\d+\b$/i,
+			floatNumb : /^\b[0-9]*\.?[0-9]{1,2}\b$/i,
+			notEmpty : /.+/
+		};
+
+		//Data validate
+		$(document).delegate('[data-validate-type]', 'focusout', function(){
+			var $this = $(this),
+				validatorType = $this.data('validate-type'),
+				checkValue = $this.val(),
+				checkTag = $this.prop('tagName');
+
+			if(validatorType !== 'none'){
+				if($this.prop('tagName') == 'SELECT'){
+					if($this.find(':selected').text() !== $this.find(':first').text())
+						$this.closest('.ws-field').removeClass('data-error');
+					else
+						$this.closest('.ws-field').addClass('data-error');
+				}
+				else{
+					if((regexp[validatorType]).test(checkValue))
+						$this.closest('.ws-field').removeClass('data-error');
+					else
+						$this.closest('.ws-field').addClass('data-error');
+				}
+			}
+		});
+
+		//Ajax loader
+		var loader = {
+			loaderSelector: '#loading',
+			errorSelector: '#errorAjax',
+			getPos: function (e){
+				e.preventDefault();
+				var yPosLoad = e.pageY+7,
+					xPosLoad = e.pageX+7;
+				$loadingItem.css({'top' : yPosLoad+7, 'left' : xPosLoad+7, 'display' : 'block'});
+			},
+			on: function(){
+				var obj = this;
+				$(window).on('mousemove', function(e){
+					obj.getPos.apply(obj, e);
+				});
+			},
+			off: function(){
+				$(window).unbind('mousemove');
+			}
+		}
+		
+
+		//Constructor for AJAX forms
+		function ajaxForm(element){
+			var obj = this;
+
+			obj.datas = {};
+			obj.ajaxSend = function(){
+				$(element).live('click', function(e){
+					e.preventDefault();
+					var $this = $(this),
+						$closestForm = $this.closest('form'),
+						urlAction = $closestForm.attr('action');
+
+					$closestForm.find('[data-validate-type]').trigger('focusout');
+					
+					var formErrors = $closestForm.find('.data-error');
+
+					if(formErrors.length > 0){
+						console.log('error');
+					}
+					else{
+						$(window).on('mousemove', function(e){
+							getLoaderPos(e);
+						});
+
+						$closestForm.find('[name]').each(function(){
+							var $this = $(this),
+								elVal = $this.val();
+
+							if($this.prop('tagName') == 'SELECT' && $this.find(':selected').val() == $this.find(':first').val())
+								elVal = '';
+
+							obj.datas[$this.attr('name')] = {
+								id : $this.attr('name'),
+								val : elVal,
+								label : $this.attr('placeholder')
+							}
+						});
+
+						$.ajax({
+							type: 'GET',
+							url: $closestForm.attr('action'),
+							data: obj.datas,
+							success: function(res){
+								$loadingItem.css('display', 'none');
+								$(window).unbind('mousemove');
+								obj.ajaxSucces(res);
+							},
+							error: function(){
+								obj.ajaxError();
+							}
+						})
+					}
+
+				})
+
+			};
+			obj.ajaxSucces = function(res){
+				console.log(res);
+			};
+			obj.ajaxError = function(){
+				console.log('Ajax error');
+			};
+		};
+
+		//Add room ajax
+		var addRoom = new ajaxForm('#addRoom');
+		addRoom.ajaxSend();
+		addRoom.ajaxSucces = function(res){
+			$('#searchRoom').trigger('click');
+		}
+
+		//Save room's changes ajax
+		var saveChanges = new ajaxForm('#saveRoom');
+
+		//Search rooms ajax
+		var searchRoom = new ajaxForm('#searchRoom');
+		searchRoom.ajaxSend();
+		searchRoom.ajaxSucces = function(res){
+			$('.main-rooms tbody tr').remove();
+
+			$.each(res, function(i, value){
+				var roomItem = '<tr id='+ value._id +'>\
+					<td class="town">'+ value.town +'</td>\
+					<td class="region">'+ value.region +'</td>\
+					<td class="street">'+ value.street +'</td>\
+					<td class="numbRoom">'+ value.numbRoom +'</td>\
+					<td class="floor">'+ value.floor +'</td>\
+					<td class="buildFloors">'+ value.buildFloors +'</td>\
+					<td class="space">'+ value.space +'</td>\
+					<td class="livingSpace">'+ value.livingSpace +'</td>\
+					<td class="kitchenSpace">'+ value.kitchenSpace +'</td>\
+					<td class="balcony">'+ value.balcony +'</td>\
+					<td class="bathroom">'+ value.bathroom +'</td>\
+					<td class="condition">'+ value.condition +'</td>\
+					<td class="price">'+ value.price +'</td>\
+					<td class="auction">'+ value.auction +'</td>\
+					<td class="saleStage">'+ value.saleStage +'</td>\
+					<td class="notes">'+ value.notes +'</td>\
+					<td class="publishDate">'+ value.publishDate.faoormatedDate +'</td>\
+					<td class="actionButtons">\
+						<span class="edit" title="Изменить">Изменить</span>\
+						<span class="remove" title="Удалить">Удалить</span>\
+					</td>\
+					</tr>';
+				$('.main-rooms').append(roomItem);
+			});
+			$('#slideSearchRoomSection.arrow-right').trigger('click');
+		}
+
+		//Remove element
+		$('.remove').live('click', function(){
+			var $parentTr = $(this).closest('tr'),
+				delID = { delID : $parentTr.attr('id')};
+
+			$.ajax({
+				type: 'GET',
+				url: '/delRoom',
+				data: delID,
+				success: function(res){
+					$parentTr.fadeOut(200, function(){
+						$(this).remove();
+					});
+				},
+				error: function(){
+					console.log('Ajax error');
+				}
+			})
+		});
+
+		//Edit element
+		$('.edit').live('click', function(){
+			var $parentTr = $(this).closest('tr'),
+				$editPopup = $('#editPopup');
+
+			$editPopup.find('[name="id"]').val($parentTr.attr('id'));
+			$parentTr.find('td').each(function(i, value){
+				var $this = $(this);
+				if($this.attr('class') !== 'actionButtons'){
+					var $elementForInput = $editPopup.find('.'+$this.attr('class')),
+						typeElementForInput = $elementForInput.attr('type');
+
+					if(typeElementForInput == 'select'){
+						$elementForInput.find('option:selected').each(function(){
+							$(this).removeAttr('selected');
+						});
+						if($this.text() !== '')
+							$elementForInput.find("[value='"+$this.text()+"']").attr("selected", "selected");
+						else
+							$elementForInput.find('option:first').attr("selected", "selected");
+					}
+					else{
+						$elementForInput.val($this.text());
+					}
+				}
+			});
+			$editPopup.fadeIn();
+
+			saveChanges.ajaxSend();
+			saveChanges.ajaxSucces = function(res){
+				$parentTr.find('.town').text(res.town.val);
+				$parentTr.find('.region').text(res.region.val);
+				$parentTr.find('.street').text(res.street.val);
+				$parentTr.find('.numbRoom').text(res.numbRoom.val);
+				$parentTr.find('.floor').text(res.floor.val);
+				$parentTr.find('.buildFloors').text(res.buildFloors.val);
+				$parentTr.find('.space').text(res.space.val);
+				$parentTr.find('.livingSpace').text(res.livingSpace.val);
+				$parentTr.find('.kitchenSpace').text(res.kitchenSpace.val);
+				$parentTr.find('.balcony').text(res.balcony.val);
+				$parentTr.find('.bathroom').text(res.bathroom.val);
+				$parentTr.find('.condition').text(res.condition.val);
+				$parentTr.find('.price').text(res.price.val);
+				$parentTr.find('.auction').text(res.auction.val);
+				$parentTr.find('.saleStage').text(res.saleStage.val);
+				$parentTr.find('.notes').text(res.notes.val);
+				$editPopup.fadeOut();
+			}
+		})
+
+		//Close popup window
+		$('.close').live('click', function(){
+			clearForm($(this));
+			$(this).closest('div').fadeOut();
+		})
+
+		//Clear forms
+		function clearForm($this){
+			var $parentForm = $this.closest('form');
+
+			$parentForm.find('[name]').each(function(){
+				var $this = $(this);
+
+				if($this.prop('tagName') == 'SELECT')
+					$this.find('option:first').attr("selected", "selected");
+				else
+					$this.val('');
+				$this.closest('.ws-field').removeClass('data-error');
+			});
+		}
+
+		//Clear search form
+		$('#searshSection .clearForm').live('click', function(){
+			clearForm($(this));
+			$('#searchRoom').trigger('click');
+
+		});
+
+		//Clear addRoom form
+		$('#addItemSection .clearForm').live('click', function(){
+			clearForm($(this));
+		});
+
+		$('#slideAddRoomSection.arrow-right').live('click', function(){
+			$('#addItemSection').animate({
+				left: 0
+			}, function(){
+				$('#slideAddRoomSection').addClass('arrow-left');
+				$('#slideAddRoomSection').removeClass('arrow-right');
+			});
+		});
+
+		$('#slideAddRoomSection.arrow-left').live('click', function(){
+			clearForm($(this));
+			$('#addItemSection').animate({
+				left: -($('#addItemSection').width())-1
+			}, function(){
+				$('#slideAddRoomSection').removeClass('arrow-left');
+				$('#slideAddRoomSection').addClass('arrow-right');
+			});
+		})
+
+		$('#slideSearchRoomSection.arrow-left').live('click', function(){
+			$('#searshSection').animate({
+				right: 0
+			}, function(){
+				$('#slideSearchRoomSection').addClass('arrow-right');
+				$('#slideSearchRoomSection').removeClass('arrow-left');
+			});
+		});
+		$('#slideSearchRoomSection.arrow-right').live('click', function(){
+			$('#searshSection').animate({
+				right: -($('#searshSection').width())-1
+			}, function(){
+				$('#slideSearchRoomSection').removeClass('arrow-right');
+				$('#slideSearchRoomSection').addClass('arrow-left');
+			});
+		})
+	});
+})(jQuery);
